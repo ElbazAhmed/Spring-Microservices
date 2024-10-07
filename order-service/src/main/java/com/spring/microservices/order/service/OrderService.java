1e7dc49e0ff5132ec4ea5dc98025943a68982be6
ahmed elbaz <ahmedelbaz771@gmail.com>
2024-10-07 23:42:19 +0200

    Integrating Synchronous Communication using Spring Cloud OpenFeign

--- order-service/src/main/java/com/spring/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/spring/microservices/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.spring.microservices.order.service;
 
+import com.spring.microservices.order.client.InventoryClient;
 import com.spring.microservices.order.dto.OrderRequest;
 import com.spring.microservices.order.model.Order;
 import com.spring.microservices.order.repository.OrderRepository;
@@ -15,16 +16,25 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
-        // map OrderRequest to the Order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setPrice(orderRequest.price());
-        order.setQuantity(orderRequest.quantity());
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
 
-        orderRepository.save(order);
-        log.info("Order placed Successfully");
+        if(isProductInStock){
+            // map OrderRequest to the Order object
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setPrice(orderRequest.price());
+            order.setQuantity(orderRequest.quantity());
+
+            orderRepository.save(order);
+            log.info("Order placed Successfully");
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is not in stock");
+        }
+
+
     }
 }
